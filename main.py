22026d904ac8fcd8e2efc751fc1c5b5b9f95e34f
Brian Boates <boates@gmail.com>
2013-03-06 22:23:03 -0800

    plot data function added, feature scaling implemented, etc.

--- main.py
+++ main.py
@@ -10,32 +10,7 @@ from classes import Game
 from classes import TeamSeason
 from classes import Season
 from database import *
-
-
-def getFeatures(gameList, featureList):
-    """
-    return: dictionary of Game features
-    params:
-          gameList: list[Game]   | list of Games
-       featureList: list[string] | list of feature names
-    """
-    # initialize 2d feature array
-    features = [[] for f in featureList]
-    
-    # loop over features in Game
-    for i, f in enumerate(featureList):
-        
-        # loop over all games in gameList
-        for g in gameList:
-            
-            # append game feature to overall list
-            print i, f
-            print g
-            print g.features
-            features[i].append( g.features[f] )
-            
-    return features
-
+from utils import *
 
 def main():
     
@@ -48,45 +23,34 @@ def main():
     
     # initialize list to hold all games from all seasons
     gameList = []
-    featureList = []
+    featureList = ['pdScore', 'dStreak']
     
     # loop over all seasons
-#    for seasonName in seasonNames:
-    for seasonName in seasonNames[-1:]:
+    for seasonName in seasonNames:
         
         # get Season for seasonName
         S = getSeason(cur, seasonName)
         
         # compute projections for games in season, append to feature list
-        S.getProjections(N=5, loc='all', result='all', scheme='constant')
-        featureList.append('pdScore')
+        S.getProjections(N=10, loc='all', result='all', scheme='constant')
         
         # compute streaks for all teams' games, append to feature list
         S.getStreaks(loc='all', result='all')
-        featureList.append('dStreak')
         
         # append all games from S to gameList
         gameList += S.allGames(featureList)
         
-#    ts = S.getTeam('DET')    
-#    for g in ts.getGames():
-#        print g.features
-    
-    # get list of all games from all seasons       
-#    for g in gameList:
-#        if 'dStreak' not in g.features.keys():
-#            print '------------'
-#            print g
-#            print g.features
-    
     # get all features from all games
-    features = getFeatures(gameList, featureList)
+    features, results = getFeatures(gameList, featureList)
     
     for j in range(len(features[0])):
         for i in range(len(features)):
             print features[i][j],
-        print
-        
+        print results[j]
+       
+    makePlots(features[0], results, nbins=100)
+    
+     
         
     # close cursor and connection to MySQL db
     if cur: cur.close()
--- utils.py
+++ utils.py
@@ -56,3 +56,96 @@ def scaleFeature(f):
     scaledFeature = [(x-m)/d for x in f]
     
     return scaledFeature
+
+
+def getFeatures(gameList, featureList):
+    """
+    return: list[list[float]], list[float]
+            features matrix, results vector
+    params:
+          gameList: list[Game]   | list of Games
+       featureList: list[string] | list of feature names
+    """
+    # initialize features and results vectors
+    features, results = [], []
+    
+    # loop over features in Game
+    for i, f in enumerate(featureList):
+        
+        # initialize list for feature
+        feature = []
+        
+        # loop over all games in gameList
+        for g in gameList:
+            
+            # append feature from game
+            feature.append( g.features[f] )
+            
+            # build the results vector (only once)
+            if i == 0: results.append( g.dScore() )
+                    
+        # scale the feature
+        feature = scaleFeature(feature)
+        
+        # append game feature to overall list
+        features.append( feature )
+        
+    return features, results
+
+
+def makePlots(feature, results, nbins=100):
+    """
+    """
+    import numpy
+    
+    allpdS, home, away, tie = [], [], [], []
+    
+    for i, f in enumerate(feature):
+        if   results[i] == 0: tie.append(f)
+        elif results[i]  > 0: home.append(f)
+        elif results[i]  < 0: away.append(f)
+        allpdS.append(f)
+        
+    
+    # Create histograms for all, favor, upset, and tie
+    aHist = numpy.histogram(numpy.array(allpdS), bins=nbins,normed=False,range=(min(allpdS),max(allpdS)))
+    hHist = numpy.histogram(numpy.array(home), bins=nbins,normed=False,range=(min(allpdS),max(allpdS)))
+    vHist = numpy.histogram(numpy.array(away), bins=nbins,normed=False,range=(min(allpdS),max(allpdS)))
+    tHist = numpy.histogram(numpy.array(tie),  bins=nbins,normed=False,range=(min(allpdS),max(allpdS)))
+
+    ax, vx, hx, tx = aHist[1], vHist[1], hHist[1], tHist[1]
+    ay, vy, hy, ty = aHist[0], vHist[0], hHist[0], tHist[0]
+
+    # Scale the histograms by event occurence
+    a, v, h, t = list(ay), list(vy), list(hy), list(ty)
+    for i in range(len(ay)):
+
+        if ay[i] != 0:
+            v_scale = float(vy[i]) / float(ay[i])
+            h_scale = float(hy[i]) / float(ay[i])
+            t_scale = float(ty[i]) / float(ay[i])
+
+            v[i] = v_scale
+            h[i] = h_scale
+            t[i] = t_scale
+
+    # Write histogram for visitors (away)
+    out = open('away.hist','w')
+    for i in range(len(v)):
+        if ay[i] > 0:  # don't write if this value of dP has never occured
+            out.write(str(vx[i])+' '+str(v[i])+'\n')
+    out.close()
+
+    # Write histogram for home
+    out = open('home.hist','w')
+    for i in range(len(h)):
+        if ay[i] > 0:  # don't write if this value of dP has never occured
+            out.write(str(hx[i])+' '+str(h[i])+'\n')
+    out.close()
+
+    # Write histogram for ties
+    out = open('tie.hist','w')
+    for i in range(len(t)):
+        if ay[i] > 0:  # don't write if this value of dP has never occured
+            out.write(str(tx[i])+' '+str(t[i])+'\n')
+    out.close()
