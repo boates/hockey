8dde18236d94bacbc1438fa975c78ba74c31463e
boates <boates@squareup.com>
2013-12-27 22:42:06 -0400

    name, verbose, printing, max page functionalities

--- scrapers/nhl_scraper.py
+++ scrapers/nhl_scraper.py
@@ -8,6 +8,7 @@ class NHLScraper(object):
     """
     """
     def __init__(self, season=None, page=None, database_name=None, table_name=None):
+        self._name = 'NHLScraper'
         self._season = season
         self._page = page
         self._database_name = database_name
@@ -15,6 +16,22 @@ class NHLScraper(object):
         self._column_names = ()
         self._html = None
         self._data = None
+        self._max_page = None
+        self._verbose = True
+
+    def __str__(self):
+        s  = '<%s: ' % self.get_name()
+        s += 'season=%s, ' % self.get_season()
+        s += 'page=%s, ' % self.get_page()
+        s += 'database_name=%s, ' % self.get_database_name()
+        s += 'table_name=%s>' % self.get_table_name()
+        return s
+
+    def __repr__(self):
+        return self.__str__()
+
+    def get_name(self):
+        return self._name
 
     def get_column_names(self):
         return self._column_names
@@ -31,6 +48,9 @@ class NHLScraper(object):
     def get_page(self):
         return self._page
 
+    def get_max_page(self):
+        return self._max_page
+
     def get_database_name(self):
         return self._database_name
 
@@ -40,20 +60,37 @@ class NHLScraper(object):
     def database_table(self):
         return (self.get_database_name(), self.get_table_name())
 
+    def set_name(self, name):
+        self._name = name
+
     def set_column_names(self, column_names):
         self._column_names = column_names
 
     def set_season(self, season):
         self._season = season
+        self._data = None
 
     def set_page(self, page):
         self._page = page
+        self._data = None
+
+    def set_max_page(self, max_page):
+        self._max_page = max_page
 
     def set_database_name(self, database_name):
         self._database_name = database_name
 
     def set_table_name(self, table_name):
         self._table_name = table_name
+
+    def verbose(self):
+        self._verbose = True
+
+    def silent(self):
+        self._verbose = False
+
+    def is_verbose(self):
+        return self._verbose
 
     def get_url(self):
         raise NotImplementedError, 'must be implemented in sub-class'
